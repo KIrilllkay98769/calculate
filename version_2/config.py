f07a74a52a5ed6f8dfea3df444b76f64b7f2fea5
Kirill <lyabakhkirill@gmail.com>
2026-09-10 11:40:02 +0300

    Добален файл config.py в version_2

---
+++ version_2/config.py
@@ -0,0 +1,8 @@
+OPERATORS = {
+    "+": lambda a, b: a + b,
+    "-": lambda a, b: a - b,
+    "*": lambda a, b: a * b,
+    "/": lambda a, b: a / b,
+    "%": lambda a , b: a % b,
+    "p": lambda a , b:  2 * (a + b)
+}
